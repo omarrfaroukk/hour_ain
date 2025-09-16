bb201c4e288fd13e22576d6a3922ab5fd933b9fa
Omar Farouk <omarello2006@gmail.com>
2025-09-17 01:35:24 +0300

    user model

---
+++ lib/features/auth/model/user.dart
@@ -0,0 +1,14 @@
+class User {
+  String email;
+  String password;
+  String username;
+  int number;
+  int? id;
+
+  User({
+    required this.email,
+    required this.password,
+    required this.username,
+    required this.number,
+  });
+}
